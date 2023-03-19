4e6ab3110fc56b989c617f179012a95e657bfbda
ballapriya <126747477+ballapriya@users.noreply.github.com>
2023-03-19 21:58:55 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <footer className="tm-footer row tm-mt-small">
             <div className="col-12 font-weight-light">
                 <p className="text-center text-white mb-0 px-4 small">
-                    Copyright &copy; <b>2023</b> All rights reserved. Design By: <a rel="nofollow noopener" href="#" className="tm-footer-link">Ankush</a>
+                    Copyright &copy; <b>2023</b> All rights reserved. Design By: <a rel="nofollow noopener" href="#" className="tm-footer-link">priyanka</a>
                 </p>
             </div>
         </footer>
